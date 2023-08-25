b459a722f8338e0228a003da9b09e50f0e04a786
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-08-25 10:51:52 -0300

    Logo y se agregaro los LINKS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import logo from './../../assets/logo.png'
+import logo from "./../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-      <nav className='bg-[#001A29] text-white flex justify-between items-center h-11'>
-        <img src={logo} alt="" className='ml-4 h-6'/>
-        <ul className='flex gap-10'>
-          <li className=''>Inicio</li>
-          <li className=''>Colegios</li>
-          <li className=''>Contacto</li>
-          <li className=''>Ayuda</li>
+      <nav className="bg-[#001A29] text-white flex justify-between items-center h-11">
+        <img src={logo} alt="logo" className="ml-4 h-6" />
+        <ul className="flex gap-10">
+          <li className="">
+            <Link>Inicio</Link>
+          </li>
+          <li className="">
+            <Link>Colegios</Link>
+          </li>
+          <li className="">
+            <Link>Contacto</Link>
+          </li>
+          <li className="">
+            <Link>Ayuda</Link>
+          </li>
         </ul>
-        <ul className='flex gap-5 mr-4'>
+        <ul className="flex gap-5 mr-4">
           <li>Registrarse</li>
           <li>Ingresar</li>
         </ul>
       </nav>
-      
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
