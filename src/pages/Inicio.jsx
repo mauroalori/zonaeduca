5323836c6758efd18a1784cd10c666cb1e248255
Ksenia Jarjury <kseniadarita@gmail.com>
2023-08-29 20:07:05 -0300

    Creacion primera seccion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,8 +1,14 @@
 import Navbar from "../components/Navbar/Navbar"
+import fondo from "../assets/fondo.png"
+import '../styles/inicio.css';
 function Inicio() {
   return (
     <>
       <Navbar/>
+      <div className="contenedor">
+        <img src={fondo} alt="" className="imagen"/>
+        <p className="texto">Te ayudamos a encontrar el mejor <br /> colegio para tus hijos</p>
+      </div>
     </>
   )
 }
