834f0c11b6f58213d205fb45cdf99763f9e0c84a
Ksenia Jarjury <kseniadarita@gmail.com>
2023-09-10 12:03:43 -0300

    Agregar estilo al botón

--- src/components/Inicio/seccionPresentacion/SeccionPresentacion.jsx
+++ src/components/Inicio/seccionPresentacion/SeccionPresentacion.jsx
@@ -1,20 +1,20 @@
-import fondo from "../../../assets/fondo.png"
+import fondo1 from "../../../assets/fondo1.png"
 import fotoPresentacion from "../../../assets/fotoP.png"
 import "./seccionPresentacion.css"
 
 export default function SeccionPresentacion() {
     return (
       <>
-      <div className="contenedor bg-[#00B0E4] bg-opacity-20 h-90 flex">
+      <div className="container_presentacion relative flex bg-[#00B0E4] bg-opacity-20 w-full">
         <div className="w-1/2 flex items-center justify-center p-6">
-            <img src={fondo} alt="" className="imagen1"/>
+            <img src={fondo1} alt="" className="imagen1"/>
             <img src={fotoPresentacion} alt=""  className="imagen2"/>
         </div>
         <div className="w-1/2 flex flex-col justify-center items-center p-6">
             <div >
-            <p className="presentacion">
-              ¡Bienvenidos a ZonaEduca! <br></br>
-            </p>
+            <h3 className="titulo text-3xl">
+              ¡Bienvenidos a ZonaEduca!
+            </h3>
             <p className="presentacion">
               Somos tu guía para encontrar el mejor colegio cerca de tu hogar. Facilitamos la búsqueda al proporcionarte información detallada, comparaciones y reseñas de colegios en tu zona. ¡Encuentra la educación perfecta para tus hijos sin complicaciones en ZonaEduca!
             </p>
