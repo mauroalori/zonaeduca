e331a9c8fb853895908587a0fb58bcd4582ec9eb
Mauro Alori <mauroalori@hotmail.com>
2023-09-15 09:22:41 -0300

    se cambia la posicion y nombre de las clases de las imagenes en portada

--- src/components/Inicio/seccionPresentacion/SeccionPresentacion.jsx
+++ src/components/Inicio/seccionPresentacion/SeccionPresentacion.jsx
@@ -5,12 +5,12 @@ import "./seccionPresentacion.css"
 export default function SeccionPresentacion() {
     return (
       <>
-      <div className="container_presentacion bg-[#00B0E4] bg-opacity-20">
-        <div className="container_imagen">
-            <img src={fondo1} alt="" className="imagen1"/>
-            <img src={fotoPresentacion} alt=""  className="imagen2"/>
+      <div className="container-presentacion bg-[#00B0E4] bg-opacity-20">
+        <div className="container-imagen bg-slate-400 w-[50%] relative">
+              <img src={fondo1} alt="" className="imagen1 absolute"/>
+              <img src={fotoPresentacion} alt=""  className="imagen2 absolute"/>
         </div>
-        <div className="container_frase">
+        <div className="container-frase w-[50%]">
             <div >
             <h3 className="titulo">
               ¡Bienvenidos a ZonaEduca!
