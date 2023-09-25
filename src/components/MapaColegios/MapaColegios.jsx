895d57de9af515180e92791ef5dedbd68ea9678a
Mauro Alori <mauroalori@hotmail.com>
2023-09-25 12:07:06 -0300

    configurar el mapa y cargar clave de api mediante variables de entorno

--- src/components/MapaColegios/MapaColegios.jsx
+++ src/components/MapaColegios/MapaColegios.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 function MapaColegios() {
+  const API_KEY = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <>
-      
+      <GoogleMap 
+        zoom={12} 
+        center={{lat: -34.612181, lng: -58.441959}} 
+        mapContainerClassName="h-screen w-full"
+      >
+
+      </GoogleMap>
     </>
-  )
+  );
 }
 
 export default MapaColegios
