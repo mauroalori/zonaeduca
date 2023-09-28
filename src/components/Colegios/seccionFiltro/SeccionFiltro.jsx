3f73d63b1044fb9e4a419a20b84761f7ba2ae9e5
MeliObre <meliobre06@gmail.com>
2023-09-27 22:00:08 -0300

    Filtro

---
+++ src/components/Colegios/seccionFiltro/SeccionFiltro.jsx
@@ -0,0 +1,29 @@
+import "./filtro.css"
+function SeccionFiltro(){
+    return (
+        <div className="contenedor">
+            <p className="text">Encuentre su colegio</p>
+        <form action="">
+            <div class="conForm">
+                <div class="input-Fil">
+                    <label for="Departamento">Departamento</label>
+                    <input type="text" placeholder="Departamento" class="Departamento" />
+                </div>
+                <div class="input-Fil" >
+                    <label for="Nivel">Nivel</label>
+                    <input type="text" placeholder="Nivel" class="Nivel"/>
+                </div>
+                <div class="input-Fil">
+                    <label for="Idioma">Idioma</label>
+                    <input type="text" placeholder="Idioma" class="Idioma"/>
+                </div>
+                <div class="input-Fil">
+                    <button>Buscar</button>
+                </div>
+            </div>
+        </form>
+        </div>
+    );
+}
+
+export default SeccionFiltro;
--- src/pages/Colegios.jsx
+++ src/pages/Colegios.jsx
@@ -1,9 +1,13 @@
-
+import Navbar from "../components/Navbar/Navbar"
+import SeccionFiltro from "../components/Colegios/seccionFiltro/SeccionFiltro"
 function Colegios() {
   return (
     <>
       <h1 className="text-center">Colegios</h1>
-    </>
+      <Navbar/>
+      {/* SECCION FILTRO  */}
+      <SeccionFiltro/>
+      </>
   )
 }
 
