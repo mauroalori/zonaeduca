80417429b8e8ff4324c452b78d7a29da46a5eeb0
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-10-04 22:36:58 -0300

    arreglo de ramas

--- src/pages/Colegios.jsx
+++ src/pages/Colegios.jsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Navbar/Navbar"
 import Footer from "../components/Footer/Footer"
 import MapaColegios from "../components/MapaColegios/MapaColegios"
 import SeccionFiltro from "../components/Colegios/seccionFiltro/SeccionFiltro"
+import ListadoColegios from "../components/MapaColegios/ListaColegios/ListadoColegios"
+
 function Colegios() {
   return (
     <>
