2a9e09f85ddf2be93a92660853407eaa0526f61f
Mauro Alori <mauroalori@hotmail.com>
2023-10-13 09:25:56 -0300

    reestablecer la pagina de colegios

--- src/pages/Colegios.jsx
+++ src/pages/Colegios.jsx
@@ -3,13 +3,7 @@ import Footer from "../components/Footer/Footer"
 import MapaColegios from "../components/MapaColegios/MapaColegios"
 import SeccionFiltro from "../components/Colegios/seccionFiltro/SeccionFiltro"
 import ListadoColegios from "../components/MapaColegios/ListaColegios/ListadoColegios"
-import { useState } from "react"
-
 function Colegios() {
-
-  const [selectedDepartamento, setSelectedDepartamento] = useState("");
-  const [selectedNivel, setSelectedNivel] = useState("");
-  const [selectedIdioma, setSelectedIdioma] = useState("");
 
   return (
     <>
@@ -17,13 +11,13 @@ function Colegios() {
         <Navbar/>
 
       {/* SECCION FILTRO  */}
-      <SeccionFiltro setSelectedDepartamento={setSelectedDepartamento} setSelectedNivel={setSelectedNivel} setSelectedIdioma={setSelectedIdioma}/>
+      <SeccionFiltro/>
 
       {/* MAPA COLEGIOS */}
       <MapaColegios />
 
       {/* LISTADO COLEGIOS */}
-      <ListadoColegios selectedDepartamento={selectedDepartamento} selectedNivel={selectedNivel} selectedIdioma={selectedIdioma}/>
+      <ListadoColegios/>
         
       {/* FOOTER */}
         <Footer/>
