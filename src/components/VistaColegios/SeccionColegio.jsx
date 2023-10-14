9773bc17a3bab7098ac93245ddb22c0c0895f1a6
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-10-14 16:59:36 -0300

    Agregar VistaColegio

--- src/components/VistaColegios/SeccionColegio.jsx
+++ src/components/VistaColegios/SeccionColegio.jsx
@@ -1,13 +1,19 @@
 import { datosColegios } from "../../data/data";
 import flecha from "../../assets/gifs-iconos/flecha.png";
+import circulo from "../../assets/gifs-iconos/circulo.png";
+import caracteristica from "../../assets/gifs-iconos/caracteristica.png";
 import './SeccionColegio.css';
 
-function SeccionColegio() {
-    const colegio = datosColegios[54];
+function SeccionColegio(idColegio) {
+    // const colegio = datosColegios[idColegio];
+    const colegio = datosColegios[52];
+    // if (!colegio) {
+    //     return <div>El colegio seleccionado no existe.</div>;
+    // }
     return (
         <>
-            <div className="colegio-descrip">
-                <div className="dato-colegio">
+            <div className="dato-colegio">
+                <div className="nom-colegio">
                     <img src={flecha} alt="" className="icon-flecha" />
                     <h1 className="nombre-colegio">{colegio.nombre}</h1>
                 </div>
@@ -18,27 +24,59 @@ function SeccionColegio() {
                         ))}
                     </ul>
                 </div>
-                <div className="descripcion">
+                <div className="descripcion-colegio">
                     <div className="descrip-imagen">
                         <img src={colegio.imagen} alt="" className="img-cole" />
                     </div>
                     <div className="descrip-cole">
-                        <p className="descripcion-cole">{colegio.descripcion}</p>
+                        <p>{colegio.descripcion}</p>
                     </div>
                 </div>
-                <div>
-                    <h2 className="contacto-cole">Contacto</h2>
+                <div className="contacto">
+                    <div>
+                        <h2 className="item-cole">Contacto</h2>
+                    </div>
+                    <div>
+                        <ul className="contact-info">
+                            <li>
+                                <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
+                                <p><strong>Tel:</strong> {colegio.telefono}</p>
+                            </li>
+                            <li>
+                                <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
+                                <p><strong>Mail:</strong> {colegio.mail}</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <h2 className="contacto-cole">Caracteristicas</h2>
+                <div className="caracter-cole">
+                    <img src={caracteristica} alt="" className="icon-caracter" />
+                    <h2 className="item2-cole">Caracteristicas</h2>
                 </div>
-                <div>
-                    <h2 className="contacto-cole">Infraestructura</h2>
+                <div >
+                    <h2 className="item-cole">Infraestructura</h2>
+                    <ul className="caracteristica">
+                        {colegio.caracteristicas.map((caracteristicas, index) => (
+                            <li key={index}>
+                                <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
+                                {caracteristicas}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <div>
-                    <h2 className="contacto-cole">Idiomas & Certificaciones</h2>
+                <div className="idiomas">
+                    <h2 className="item-cole">Idiomas & Certificaciones</h2>
+                    <ul className="idiomas-list">
+                        {colegio.idiomas.map((idioma, index) => (
+                            <li key={index} className="idiomas-item">
+                                <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
+                                {idioma}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
+
 
         </>
     )
