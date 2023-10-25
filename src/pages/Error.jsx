6cb0684d7ddf247af48f36ee7d6a2c25a36e5ce6
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-10-25 12:07:14 -0300

    se agrego flecha hacia arriba y modificacion del formulario

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,7 +7,7 @@ function Error() {
         <h1 className="text-center font-semibold text-4xl text-red-500">ERROR DE RUTA, VUELVA A LA PAGINA :C</h1>
 
         <Link to={"/"}>
-        <p className="text-center m-auto mt-10 p-2 text-white rounded-lg w-56 bg-[#00729A] hover:bg-[#175a72] duration-200">
+        <p className="text-center m-auto mt-10 p-2 text-white rounded-lg w-56 bg-[#00729A] hover:bg-[#175a72] duration-200 ">
         VOLVER AL INICIO
         </p>
         </Link>
