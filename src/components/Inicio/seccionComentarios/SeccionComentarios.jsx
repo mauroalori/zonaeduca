dbbd6c6031ec62512889a185e0ba00ad3f0e43f4
Mauro Alori <mauroalori@hotmail.com>
2023-12-04 14:49:59 -0300

    formulario de agregar comentarios

--- src/components/Inicio/seccionComentarios/SeccionComentarios.jsx
+++ src/components/Inicio/seccionComentarios/SeccionComentarios.jsx
@@ -1,6 +1,13 @@
+import { button } from "@material-tailwind/react"
 import SliderComentarios from "./swiperComentarios/SliderComentarios"
+import { Link } from "react-router-dom"
 
 function SeccionComentarios() {
+
+  const manejarEnvio = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
 
@@ -8,6 +15,20 @@ function SeccionComentarios() {
 
     <SliderComentarios/>
     
+    <div className="flex flex-col items-center pb-14">
+      <h2 className="text-center text-xl text-[#00405B] mb-10">¡Dejanos tu comentario!</h2>
+      <form onSubmit={manejarEnvio} id="form-comentario" className="flex flex-col gap-5 items-center">
+        <textarea name="comentario" cols="20" rows="10" placeholder="Escribe tu comentario" className="w-full border-brown-300"></textarea>
+        {false ? (
+          <input type="submit" value="Enviar" className="m-auto bg-[#3DCCFF] text-white w-20 p-1 rounded-md hover:bg-[#1e8eb7] duration-200 cursor-pointer"/>
+        ):(
+          <Link to={"/Login"}>
+            <button className="m-auto bg-[#3DCCFF] text-white py-2 px-4 rounded-md hover:bg-[#1e8eb7] duration-200 cursor-pointer">Iniciar Sesion</button>
+          </Link>
+        )}
+      </form>
+    </div>
+
     
     </div>
   )
